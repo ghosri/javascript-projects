dd4103e7d634068c3dec3f2f336c9d4494120d8c
ghosri <abulgazievsamat4@gmail.com>
2024-06-22 14:22:48 -0400

    arra

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -4,10 +4,20 @@ let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
-
+let foodCabinet = food.split(",").sort()
+console.log(foodCabinet)
+let equipmentCabinet = equipment.split(",").sort()
+console.log(equipmentCabinet)
+let petsCabinet = pets.split(",").sort()
+console.log(petsCabinet)
+let sleepAidsCabinet = sleepAids.split(",").sort()
+console.log(sleepAidsCabinet)
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
-
+let cargoHold = [];
+cargoHold = cargoHold.concat(foodCabinet, equipmentCabinet, petsCabinet, sleepAidsCabinet)
+console.log(cargoHold )
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+const input = require('readline-sync');
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 
