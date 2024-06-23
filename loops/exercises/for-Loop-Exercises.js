d9ba36bfa5c6bc0b83f2d71ac4bd40871d96dbfe
ghosri <abulgazievsamat4@gmail.com>
2024-06-23 10:20:31 -0400

    loops

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,7 +3,21 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
-
+for(let i = 0; i <= 20; i++){
+  console.log(i)
+}
+for(let i = 3; i <= 29; i+=2){
+  console.log(i)
+}
+for(let i = 12; i >=-14; i-=2){
+  console.log(i)
+}
+for(let i = 50; i >= 20; i--){
+  if(i % 3 == 0){
+    console.log(i)
+  }
+  else{}
+}
 
 
 
@@ -14,11 +28,31 @@ Initialize two variables to hold the string “LaunchCode” and the array [1, 5
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+let var1 = "LaunchCode"
+let var2 = [1, 5, 'LC101', 'blue', 42]
 
-
+for(let i = 0; i < var2.length; i++){
+  console.log(var2[i])
+}
+for(let i = 10; i >= 0; i--){
+  console.log(var1[i])
+}
 
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+let arr1 = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104]
+let even = []
+let odd =[]
+for(let i = 0; i < arr1.length; i++){
+  if(arr1[i] % 2 == 0){
+    even.push(arr1[i])
+  }
+  else{
+    odd.push(arr1[i])
+  }
+}
+console.log(even)
+console.log(odd)
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,12 +1,14 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-
+let initFuel;
+let crew;
+let maxAltitude;
 
 
 
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
-
+const input = require('readline-sync');
 
 
 
