da354d142dfa95d9cc858a4724ac17a655df783b
ghosri <abulgazievsamat4@gmail.com>
2024-07-15 20:16:52 -0400

    unit test studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,41 @@
 
 let launchcode = {
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput(input){
+        if(input % 2 == 0 && input % 3 == 0 && input % 5 == 0){
+            return "LaunchCode Rocks!"
+        }
+        else if(input % 2 != 0 && input % 3 != 0 && input % 5 != 0){
+            return "Rutabagas! That doesn't work."
+        }
+        else if(input % 2 == 0 && input % 3 == 0){
+            return "LaunchCode!"
+        }
+        else if(input % 3 == 0 && input % 5 == 0){
+            return "Code Rocks!"
+        }
+        else if(input % 2 == 0 && input % 5 == 0){
+            return "Launch Rocks! (CRASH!!!!)"
+        }
+        else if(input % 2 == 0){
+            return "Launch!"
+        }
+        else if(input % 3 == 0){
+            return "Code!"
+        }
+        else if(input % 5 == 0){
+            return "Rocks!"
+        }
+
+
+
+    }
   
 }
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,59 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
-  // Write your unit tests here!
+  test("wrong company", function(){
+    let output = launchcode.organization
+    expect(output).toEqual("nonprofit")
+  })
+  test("wrong CEO", function(){
+    let output = launchcode.executiveDirector
+    expect(output).toEqual("Jeff")
+  })
+  test("not enough cool employees", function(){
+    let output = launchcode.percentageCoolEmployees
+    expect(output).toEqual(100)
+  })
+  test("programs offered is not complete", function(){
+    let output = launchcode.programsOffered
+    expect(output[0]).toEqual("Web Development")
+    expect(output[1]).toEqual("Data Analysis")
+    expect(output[2]).toEqual("Liftoff")
+    expect(output.length).toEqual(3)
+  })
   
 });
+
+describe("testing the launchcode method", function(){
+  test("divisible by 2", function(){
+    let output = launchcode.launchOutput(2)
+    expect(output).toEqual("Launch!")
+  })
+  test("divisible by 3", function(){
+    let output = launchcode.launchOutput(9)
+    expect(output).toEqual("Code!")
+  })
+  test("divisible by 5", function(){
+    let output = launchcode.launchOutput(25)
+    expect(output).toEqual("Rocks!")
+  })
+  test("divisible by 2 and 3", function(){
+    let output = launchcode.launchOutput(12)
+    expect(output).toEqual("LaunchCode!")
+  })
+  test("divisible by 3 and 5", function(){
+    let output = launchcode.launchOutput(15)
+    expect(output).toEqual("Code Rocks!")
+  })
+  test("divisible by 2 and 5", function(){
+    let output = launchcode.launchOutput(10)
+    expect(output).toEqual("Launch Rocks! (CRASH!!!!)")
+  })
+  test("divisible by 2, 3 and 5", function(){
+    let output = launchcode.launchOutput(30)
+    expect(output).toEqual("LaunchCode Rocks!")
+  })
+  test("not a number divisible by 2, 3 and 5", function(){
+    let output = launchcode.launchOutput(31)
+    expect(output).toEqual("Rutabagas! That doesn't work.")
+  })
+})
