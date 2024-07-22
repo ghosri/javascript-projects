7c0a4f6f5181c0938e753c968d19fda3b710cc23
ghosri <abulgazievsamat4@gmail.com>
2024-07-22 16:39:15 -0400

    classes

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,46 @@
 // Define your Book class here:
-
+class Book{
+    constructor(title, author, copyright, isbn, pages, checkOut, discarded){
+        this.title = title;
+        this.author = author;
+        this.copyright = copyright;
+        this.isbn = isbn;
+        this.pages = pages;
+        this.checkOut = checkOut;
+        this.discarded = discarded
+    }
+    checkOut(uses = 1){
+        this.checkOut += uses
+    }
+}
 
 // Define your Manual and Novel classes here:
+class Manual extends Book{
+    constructor(title, author, copyright, isbn, pages, checkOut, discarded){
+        super(title, author, copyright, isbn, pages, checkOut, discarded)
+    }
+    remove(copyright){
+        if(2024-copyright > 5){
+            return discarded = true
+        }
+    }
+}
 
+class Novel extends Book{
+    constructor(title, author, copyright, isbn, pages, checkOut, discarded){
+        super(title, author, copyright, isbn, pages, checkOut, discarded)
+    }
+    remove(copyright){
+        if(2024-copyright > 5){
+            return discarded = true
+        }
+    }
+}
 
 // Declare the objects for exercises 2 and 3 here:
-
+let pride = new Novel("Pride and Prejudice", "Jane Austen", 1813, 1111111111111, 432, 32, false)
+let ship = new Manual("Top Secret Shuttle Building Manual", "Redacted", 2013, "0000000000000", 1147, 1, false)
 
 // Code exercises 4 & 5 here:
+console.log(pride.checkOut(5))
+console.log(pride.remove())
