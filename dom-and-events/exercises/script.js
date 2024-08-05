1920c2437e228782ba2de9c8077b0ab35147d9b1
ghosri <abulgazievsamat4@gmail.com>
2024-08-05 18:42:46 -0400

    dom and events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,9 +2,27 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
+    const confirm = document.getElementById("abortConfirm");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+     missionAbort.addEventListener("mouseover", event =>{
+        event.target.style.backgroundColor = "red";
+     })
+
+     missionAbort.addEventListener("mouseout", event =>{
+        event.target.style.backgroundColor = "";
+     })
+
+     missionAbort.addEventListener("click", event =>{
+        if(window.confirm("Are you sure you want to abort?")){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+        
+     })
 }
 
 window.addEventListener("load", init);
